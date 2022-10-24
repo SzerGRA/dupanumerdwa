456fa39f800d97f60ba97eafe8877b86e21920f6
SzerGRA <szymon.wodzinski@o2.pl>
2022-10-24 23:00:36 +0200

    deleted not usefull stuff

--- src/main/java/szer/dupanumerdwa/Dupanumerdwa.java
+++ src/main/java/szer/dupanumerdwa/Dupanumerdwa.java
@@ -3,7 +3,6 @@ package szer.dupanumerdwa;
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
@@ -12,12 +11,9 @@ import net.minecraft.world.level.material.Material;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
@@ -29,7 +25,6 @@ import szer.dupanumerdwa.core.ModItems;
 
 import static szer.dupanumerdwa.core.ModItems.TAB;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(Dupanumerdwa.MODID)
 public class Dupanumerdwa {
 
@@ -41,7 +36,6 @@ public class Dupanumerdwa {
 
     // Creates a new Block with the id "dupanumerdwa:example_block", combining the namespace and path
     public static final RegistryObject<Block> EXAMPLE_BLOCK = BLOCKS.register("example_block", () -> new Block(BlockBehaviour.Properties.of(Material.STONE)));
-    // Creates a new BlockItem with the id "dupanumerdwa:example_block", combining the namespace and path
     public static final RegistryObject<Item> EXAMPLE_BLOCK_ITEM = ITEMS.register("example_block", () -> new BlockItem(EXAMPLE_BLOCK.get(), new Item.Properties().tab(TAB)));
 
     public Dupanumerdwa() {
